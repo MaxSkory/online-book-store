f435905cb4a038fa4ad43efec6c6cef4e9b76b9a
Max Skory <devisrush@gmail.com>
2023-10-12 12:47:40 +0300

    unused imports were removed

--- src/main/java/mskory/bookstore/OnlineBookStoreApplication.java
+++ src/main/java/mskory/bookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,7 @@
 package mskory.bookstore;
 
-import java.math.BigDecimal;
-import mskory.bookstore.model.Book;
-import mskory.bookstore.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
